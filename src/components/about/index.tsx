ee552617fd48cf03d14e389e87e5ba971ae2d90d
Ronan Morais <61247547+nanza86@users.noreply.github.com>
2023-01-26 01:09:41 -0300

    about

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -9,8 +9,15 @@ const About = () => {
   return (
     <section className="container overflow-hidden flex max-w-full h-screen bg-stone-900 bg-contain relative justify-center items-center">
       <div className="absolute z-10 w-full flex">
-        <div className="w-1/2 h-screen">
-          <div><Image></Image></div>
+        <div className="w-1/2 h-screen p-32">
+          <div className="border-8 p-20">
+            <Image
+              src="/images/avatar.png"
+              width="200"
+              height="200"
+              alt="Ronan Morais"
+            />
+          </div>
         </div>
         <div className="w-1/2 flex h-screen">
           <AboutSlider />
