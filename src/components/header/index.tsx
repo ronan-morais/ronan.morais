b5625518401b3bc5df85254aa6048ee94136d666
Ronan Morais <61247547+nanza86@users.noreply.github.com>
2023-01-26 01:27:11 -0300

    text size

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -19,13 +19,15 @@ const Header = () => {
       >
         <div className="">
           <Logo />
-          <h2 className="text-white font-dmserif text-2xl lg:text-4xl mt-8">Welcome to</h2>
+          <h2 className="text-white font-dmserif text-1xl md:text-2xl lg:text-3xl xl:text-4xl mt-8">
+            Welcome to
+          </h2>
           <h1 className="text-white font-montserrat font-black text-7xl md:text-9xl lg:text-[170px] leading-[0.9em] uppercase tracking-tighter">
             Ronan
             <br />
             Morais
           </h1>
-          <h2 className="text-white font-dmserif text-2xl lg:text-3xl mt-1">
+          <h2 className="text-white font-dmserif  text-1xl md:text-2xl lg:text-3xl xl:text-4xl mt-1">
             Graphic Designer and Front-End Developer Website
           </h2>
         </div>
