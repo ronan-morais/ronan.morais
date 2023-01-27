59d48f7af4062d311bd60c700f6318a764d18826
Ronan Morais <61247547+nanza86@users.noreply.github.com>
2023-01-27 02:01:29 -0300

    nextjs Image

--- src/components/about/slider/index.tsx
+++ src/components/about/slider/index.tsx
@@ -24,35 +24,41 @@ const AboutSlider = () => {
           alt=""
         />
       </SwiperSlide>
-      {/* <SwiperSlide
-        className="bg-[url(/images/about/1-min.jpg)] bg-cover w-full h-full bg-right md:bg-center lg:bg-right"
-        style={{ filter: "grayscale(100%)" }}
-      >
-        &nbsp;
-      </SwiperSlide> */}
-      <SwiperSlide
-        className="bg-[url(/images/about/2-min.jpg)] bg-cover w-full h-full bg-right"
-        style={{ filter: "grayscale(100%)" }}
-      >
-        &nbsp;
+
+      <SwiperSlide className="w-full h-full">
+        <Image
+          src="/images/about/2-min.jpg"
+          fill
+          style={{ objectFit: "cover", filter: "grayscale(100%)" }}
+          alt=""
+        />
       </SwiperSlide>
-      <SwiperSlide
-        className="bg-[url(/images/about/3-min.jpg)] bg-cover w-full h-full bg-left"
-        style={{ filter: "grayscale(100%)" }}
-      >
-        &nbsp;
+
+      <SwiperSlide className="w-full h-full">
+        <Image
+          src="/images/about/3-min.jpg"
+          fill
+          style={{ objectFit: "cover", filter: "grayscale(100%)" }}
+          alt=""
+        />
       </SwiperSlide>
-      <SwiperSlide
-        className="bg-[url(/images/about/4-min.jpg)] bg-cover w-full h-full bg-left"
-        style={{ filter: "grayscale(100%)" }}
-      >
-        &nbsp;
+
+      <SwiperSlide className="w-full h-full">
+        <Image
+          src="/images/about/4-min.jpg"
+          fill
+          style={{ objectFit: "cover", filter: "grayscale(100%)" }}
+          alt=""
+        />
       </SwiperSlide>
-      <SwiperSlide
-        className="bg-[url(/images/about/5-min.jpg)] bg-cover w-full h-full bg-center"
-        style={{ filter: "grayscale(100%)" }}
-      >
-        &nbsp;
+
+      <SwiperSlide className="w-full h-full">
+        <Image
+          src="/images/about/5-min.jpg"
+          fill
+          style={{ objectFit: "cover", filter: "grayscale(100%)" }}
+          alt=""
+        />
       </SwiperSlide>
     </Swiper>
   );
