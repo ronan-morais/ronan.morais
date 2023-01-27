30c723aead467484a379e6f21bbb5657437989e8
Ronan Morais <61247547+nanza86@users.noreply.github.com>
2023-01-27 13:11:57 -0300

    responsive

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -7,26 +7,29 @@ const About = () => {
   const { scrollYProgress } = useScroll();
 
   return (
-    <section className="container snap-start flex-col md:flex-row overflow-hidden flex max-w-full h-screen bg-stone-900 bg-contain relative justify-center items-center">
-      <div className="w-full md:w-1/2 h-full">
-        <div className="p-12 md:p-20 lg:p-36 lg:pr-20 xl:pr-36 lg:h-screen text-white">
-          <h3 className="text-3xl md:text-4xl lg:text-6xl font-dmserif py-4 lg:py-10">About me</h3>
-          <div className="text-gray-400 font-montserrat text-xs sm:text-base md:text-lg lg:text-lg xl:text-xl">
+    <section className="container snap-start shrink flex-col md:flex-row overflow-hidden flex max-w-full h-screen bg-stone-900 bg-contain relative justify-center items-center">
+      <div className="w-full md:w-1/2">
+        <div className="p-12 sm:p-20 md:p-20 lg:p-36 lg:pr-20 xl:pr-36 lg:h-screen text-white">
+          <h3 className="text-3xl md:text-4xl lg:text-5xl font-dmserif py-4 lg:py-10">
+            About me
+          </h3>
+          <div className="text-gray-400 font-montserrat text-xs sm:text-base md:text-lg lg:text-lg">
             My name is Ronan Alves de Morais, I am 36 years old and Brazilian
             from Belo Horizonte, MG.
             <br />
-            <br />I graduated as a graphic designer at INAP College, in 2011, and
-            since then I have been working with branding, visual identity
+            <br />I graduated as a graphic designer at INAP College, in 2011,
+            and since then I have been working with branding, visual identity
             concept and creation of printed graphic pieces, but I love code and
-            I migrate to UX/UI and dedicated myself as a front-end developer, and I have good skills
-            in Html, Css, Javascript, with focus on React and Nextjs.
+            I migrate to UX/UI and dedicated myself as a front-end developer,
+            and I have good skills in Html, Css, Javascript, with focus on React
+            and Nextjs.
             <br />
             <br />I love people, technology, forests and &quot;pão de
             queijo&quot;.
           </div>
         </div>
       </div>
-      <div className="w-full md:w-1/2 flex h-[100vh] md:h-screen">
+      <div className="w-full md:w-1/2 flex h-full md:h-screen">
         <AboutSlider />
       </div>
     </section>
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -15,7 +15,7 @@ const Header = () => {
   const brandPosition = useTransform(scrollYProgress, [0, 1], ["25%", "180%"]);
 
   return (
-    <section className="container snap-start overflow-hidden flex max-w-full h-screen bg-black bg-contain relative justify-center items-center">
+    <section className="container snap-start shrink-0 overflow-hidden flex max-w-full h-screen bg-black bg-contain relative justify-center items-center">
       <motion.div
         className="absolute z-10 w-full flex pl-12 sm:px-20 lg:px-28"
         style={{ top: brandPosition }}
@@ -23,17 +23,19 @@ const Header = () => {
         <div className="">
           <Logo />
           <motion.h1
-            className="text-white pt-7 lg:absolute overflow-hidden lg:whitespace-nowrap font-montserrat font-black text-[19vw] lg:text-[300px] leading-[0.9em] uppercase tracking-tight"
+            className="text-white pt-7 lg:absolute overflow-hidden lg:whitespace-nowrap font-montserrat font-black text-[18vw] lg:text-[300px] leading-[0.9em] uppercase tracking-tight"
             style={{
               left: left,
             }}
           >
-            Ronan Morais <span className="hidden lg:inline-flex">Ronan Morais Ronan Morais Ronan Morais Ronan Morais
-            Ronan Morais Ronan Morais Ronan Morais Ronan Morais Ronan Morais</span>
+            Ronan Morais{" "}
+            <span className="hidden lg:inline-flex">
+              Ronan Morais Ronan Morais
+            </span>
           </motion.h1>
           <h2 className="text-white pt-3 lg:pt-[300px] lg:absolute font-dmserif text-xl md:text-2xl lg:text-4xl xl:text-5xl mt-1">
-            Front-End Developer <br className="md:hidden" />
-            and Graphic Designer
+            Graphic Designer and <br className="md:hidden" />
+            Front-End Developer
           </h2>
         </div>
       </motion.div>
@@ -42,7 +44,7 @@ const Header = () => {
         style={{ opacity: opacity }}
       ></motion.div>
       <motion.div
-        className="w-screen h-screen bg-cover bg-[url(/images/bg-min.jpg)]"
+        className="w-screen h-screen bg-cover bg-[url(/images/bg-min.jpg)] bg-center"
         style={{ backgroundPositionY: bg }}
       ></motion.div>
     </section>
