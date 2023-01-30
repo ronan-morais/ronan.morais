e73113468a829c12f36c7a38d572ce03cb164b2c
Ronan Morais <61247547+nanza86@users.noreply.github.com>
2023-01-30 00:19:08 -0300

    favicon

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -1,6 +1,6 @@
 const Contact = () => {
   return (
-    <section className="container flex max-w-full h-screen bg-[url(/images/bgworks.jpg)] bg-[#DEDDED] bg-right-bottom bg-no-repeat bg-size-[100%]">
+    <section className="container flex max-w-full h-screen bg-[url(/images/bgorks.jpg)] bg-[red] bg-right-bottom bg-no-repeat bg-size-[100%]">
       section
     </section>
   );
