9e276f2c2686de672953927040816224e2be9f74
Ronan Morais <61247547+nanza86@users.noreply.github.com>
2023-01-30 01:19:01 -0300

    black favico

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -1,7 +1,9 @@
 const Contact = () => {
   return (
     <section className="container flex max-w-full h-screen bg-[url(/images/bgorks.jpg)] bg-[red] bg-right-bottom bg-no-repeat bg-size-[100%]">
-      section
+      <h1 className="text-white pt-7 lg:absolute overflow-hidden lg:whitespace-nowrap font-montserrat font-black text-[18vw] lg:text-[300px] leading-[0.9em] uppercase tracking-tight">
+        Let&lsquo;s Talk!
+      </h1>
     </section>
   );
 };
